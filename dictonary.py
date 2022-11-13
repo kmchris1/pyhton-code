e340b6552612a72ab5026fb8da85692b605b437f
C K <christellekamden@gmail.com>
2022-11-12 21:23:18 -0500

    dictionary

---
+++ dictonary.py
@@ -0,0 +1,10 @@
+#print out the last name of second employee
+
+d = {"employees":
+                [{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter" }]}
+
+print(d["employees"][1]["lastName"])
